7751b9a8be4afb2e096a04c1c9a15195560bc59e
David Casale <dcasale@umd.edu>
2020-05-01 13:21:24 -0400

    Add project files.

---
+++ DACStore/DACStore.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Runtime.InteropServices;
+
+namespace DACStore
+{
+    public partial class DACStore : ServiceBase
+    {
+        private readonly EventLog ev;
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(System.IntPtr handle, ref ServiceStatus serviceStatus);
+
+        public DACStore()
+        {
+            InitializeComponent();
+
+            ev = new EventLog();
+            if (!EventLog.SourceExists("DACSource"))
+                EventLog.CreateEventSource("DACSource", "DACLog");
+            ev.Source = "DACSource";
+            ev.Log = "DACLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ServiceStatus serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_START_PENDING,
+                dwWaitHint = 100000
+            };
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
+            
+            ev.WriteEntry("Starting DACStore.");
+
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(ServiceHandle, ref serviceStatus); 
+        }
+
+        protected override void OnStop()
+        {
+            // Update the service state to Stop Pending.
+            ServiceStatus serviceStatus = new ServiceStatus
+            {
+                dwCurrentState = ServiceState.SERVICE_STOP_PENDING,
+                dwWaitHint = 100000
+            };
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
+
+            ev.WriteEntry("Stopping DACStore.");
+
+            // Update the service state to Stopped.
+            serviceStatus.dwCurrentState = ServiceState.SERVICE_STOPPED;
+            SetServiceStatus(ServiceHandle, ref serviceStatus);
+        }
+    }
+}
---
+++ DACStore/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ServiceProcess;
+
+namespace DACStore
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new DACStore()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
